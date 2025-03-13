de93ae8e9bdca635b4075d7f19edcc61947580c1
elMestre <carmesce@gmail.com>
2025-03-13 13:46:58 +0100

    solution

--- 250313-bola-magica.py
+++ 250313-bola-magica.py
@@ -1,25 +1,31 @@
-# Utilizando los principios de programación estructurada:
-# Cada vez que arranque el programa, dale al usuario una respuesta al azar
+import random
 
-arrayRespuestas = [
-    'Es cierto.',
-    'Es decididamente así.',
-    'Sin lugar a dudas.',
-    'Sí, definitivamente.',
-    'Puedes confiar en ello.',
-    'Como yo lo veo, sí.',
-    'Lo más probable.',
-    'Perspectiva buena.',
-    'Sí.',
-    'Las señales apuntan a que sí.',
-    'Respuesta confusa, vuelve a intentarlo.',
-    'Vuelve a preguntar más tarde.',
-    'Mejor no decirte ahora.',
-    'No se puede predecir ahora.',
-    'Concéntrate y vuelve a preguntar.',
-    'No cuentes con ello.',
-    'Mi respuesta es no.',
-    'Mis fuentes dicen que no.',
-    'Las perspectivas no son muy buenas.',
-    'Muy dudoso.'
-]
+
+def main():
+    respuestas = [
+        'Es cierto.',                               # 0
+        'Es decididamente así.',                    # 1
+        'Sin lugar a dudas.',                       # 2
+        'Sí, definitivamente.',                     # 3
+        'Puedes confiar en ello.',                  # 4
+        'Como yo lo veo, sí.',                      # 5
+        'Lo más probable.',                         # 6
+        'Perspectiva buena.',                       # 7
+        'Sí.',                                      # 8
+        'Las señales apuntan a que sí.',            # 9
+        'Respuesta confusa, vuelve a intentarlo.',  # 10
+        'Vuelve a preguntar más tarde.',            # 11
+        'Mejor no decirte ahora.',                  # 12
+        'No se puede predecir ahora.',              # 13
+        'Concéntrate y vuelve a preguntar.',        # 14
+        'No cuentes con ello.',                     # 15
+        'Mi respuesta es no.',                      # 16
+        'Mis fuentes dicen que no.',                # 17
+        'Las perspectivas no son muy buenas.',      # 18
+        'Muy dudoso.'                               # 19
+    ]
+
+    print(random.choice(respuestas))
+
+
+main()
